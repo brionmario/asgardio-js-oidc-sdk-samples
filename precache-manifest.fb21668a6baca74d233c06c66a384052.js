e4de4bdc4563bd43ed077bcd3e0d8491eb0ace64
Brion Mario <info@brionmario.com>
2020-12-07 03:28:41 +0530

    Initial commit

---
+++ precache-manifest.fb21668a6baca74d233c06c66a384052.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee6240869dba54153493507cbde3bde4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "429fedf1b751acf3a029",
+    "url": "/static/css/main.d94c48d6.chunk.css"
+  },
+  {
+    "revision": "f4e0a77afbc3567e7d85",
+    "url": "/static/js/2.a8d00439.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.a8d00439.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "429fedf1b751acf3a029",
+    "url": "/static/js/main.fb5b6aab.chunk.js"
+  },
+  {
+    "revision": "8b09ba2026413d613d1715865551d148",
+    "url": "/static/js/main.fb5b6aab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e9f67b2b97769052742",
+    "url": "/static/js/runtime-main.bf11dcf5.js"
+  }
+]);
